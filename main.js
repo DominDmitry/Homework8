c9b0f8b98215ca91d64d9ef884fa57186e0acfac
DominDmitry <112818952+DominDmitry@users.noreply.github.com>
2023-01-25 07:55:17 +0200

    fin

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ for(i=0;i<n;i++){
 console.log(arr)
 let sortedArr = arr.sort((a, b) => a - b);
 let removed = sortedArr.splice(1, 3);
-alert(`Массив отсортирован по возростанию без 2/3/4 элементов: ${removed}`)
+alert(`Массив отсортирован по возростанию без 2/3/4 элементов: ${removed}`);
